b03ebc74a106d20e3653f7b4dc470e0522ed8bab
soumyak_bhattacharyya <soumyak.bhattacharyya@rubiconred.com>
2018-05-08 12:43:06 +0530

    introduced lambda expression

--- src/main/java/java8lambda/Greeter.java
+++ src/main/java/java8lambda/Greeter.java
@@ -6,6 +6,10 @@ public class Greeter {
 		greeting.perform();
 	}
 
+	public int add(AddFunction addFunction) {
+		return addFunction.add(5, 6);
+	}
+
 	public static void main(String[] args) {
 
 		Greeter greeter = new Greeter();
@@ -16,6 +20,29 @@ public class Greeter {
 		// behavior is being passed as an argument to the method
 		greeter.greet(actOfGreeting);
 
+		// equivalent implementation
+		ActOfGreeting actOfGreeting2 = () -> System.out.println("Hello World :-D");
+
+		greeter.greet(actOfGreeting2);
+
+		AddFunction addFunction = (i, j) -> {
+			return i + j;
+		};
+		
+		System.out.println(addFunction.add(8, 9));
+
 	}
+
+	// target behavior
+
+	/**
+	 * aVariable = public void myMethod(){ // further code logic}
+	 * 
+	 * public - makes sense if the method is a member of a class name - is
+	 * unnecessary if the function body is deferrable return type - can be guessed
+	 * from the return statement of a function
+	 * 
+	 * () -> {// some code}
+	 */
 
 }
