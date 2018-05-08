70ce877dbb2defdee8db5754b9fe7e77e0e2bfbc
soumyak_bhattacharyya <soumyak.bhattacharyya@rubiconred.com>
2018-05-08 10:57:47 +0530

    updated design

--- src/test/java/java8lambda/AppTest.java
+++ src/test/java/java8lambda/AppTest.java
@@ -1,17 +1,11 @@
 package java8lambda;
 
-import org.junit.Assert;
 import org.junit.Test;
-
-import java8lambda.App;
 
 public class AppTest {
 
 	@Test
 	public void testLengthOfTheUniqueKey() {
-
-		App obj = new App();
-		Assert.assertEquals(36, obj.generateUniqueKey().length());
 
 	}
 }
