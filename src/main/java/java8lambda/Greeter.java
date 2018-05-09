985de58b380dbf96739ea302a98e226c47494462
soumyak_bhattacharyya <soumyak.bhattacharyya@rubiconred.com>
2018-05-09 16:18:12 +0530

    refactored greeter class

--- src/main/java/java8lambda/Greeter.java
+++ src/main/java/java8lambda/Greeter.java
@@ -6,10 +6,6 @@ public class Greeter {
 		greeting.perform();
 	}
 
-	public int add(AddFunction addFunction) {
-		return addFunction.add(5, 6);
-	}
-
 	public static void main(String[] args) {
 
 		Greeter greeter = new Greeter();
